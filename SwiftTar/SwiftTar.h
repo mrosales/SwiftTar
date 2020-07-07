ce93afe80faaefd462d4b8f9490217805392e272
Micah Rosales <mrosales@users.noreply.github.com>
2020-07-06 21:07:29 -0500

    Initial Commit

---
+++ SwiftTar/SwiftTar.h
@@ -0,0 +1,19 @@
+//
+//  SwiftTar.h
+//  SwiftTar
+//
+//  Created by Micah Rosales on 7/6/20.
+//  Copyright © 2020 Micah Rosales. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftTar.
+FOUNDATION_EXPORT double SwiftTarVersionNumber;
+
+//! Project version string for SwiftTar.
+FOUNDATION_EXPORT const unsigned char SwiftTarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftTar/PublicHeader.h>
+
+
